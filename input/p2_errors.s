75b19793c569176cc28c25d00128819601b693b0
Class Account <cs61c-tk@hive2.CS.Berkeley.EDU>
2016-02-19 00:37:08 -0800

    fixed inconsistencies in ref files

--- input/p2_errors.s
+++ input/p2_errors.s
@@ -1,6 +1,8 @@
 		addiu $t0, $t3, $t3				# not a number
 label:	jal								# no label
 		ori $t2, $99, 0xAB				# invalid register
+
+# Things to ignore
 		bne $t0, $t1, not_found			# nonexistant label
 		addiu $t3 $t5 0x80808080		# number too large
 
