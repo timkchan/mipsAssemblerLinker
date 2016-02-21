b9ea60155cd34564fb964d0b9bdb28b823e0b13d
timkchan <timchan@berkeley.edu>
2016-02-21 11:47:38 -0800

    addind extral test file

---
+++ valgrindallin.sh
@@ -0,0 +1,10 @@
+./run-valgrind ./assembler input/simple.s out/simple.int out/simple.out
+./run-valgrind ./assembler input/combined.s out/combined.int out/combined.out
+./run-valgrind ./assembler input/comments.s out/comments.int out/comments.out
+./run-valgrind ./assembler input/imm.s out/imm.int out/imm.out
+./run-valgrind ./assembler input/jumps.s out/jumps.int out/jumps.out
+./run-valgrind ./assembler input/labels.s out/labels.int out/labels.out
+./run-valgrind ./assembler input/p1_errors.s out/p1_errors.int out/p1_errors.out
+./run-valgrind ./assembler input/p2_errors.s out/p2_errors.int out/p2_errors.out
+./run-valgrind ./assembler input/pseudo.s out/pseudo.int out/pseudo.out
+./run-valgrind ./assembler input/rtypes.s out/rtypes.int out/rtypes.out
