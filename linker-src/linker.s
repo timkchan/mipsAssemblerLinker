4a51e8de3a227c8261555a4ed528050bf0f9c964
timkchan <timchan@berkeley.edu>
2016-03-07 14:45:17 -0800

    done

--- linker-src/linker.s
+++ linker-src/linker.s
@@ -48,7 +48,8 @@ hex_buffer:		.space 10
 write_machine_code:
 	# You may need to save additional items onto the stack. Feel free to
 	# change this part.
-	addiu $sp, $sp, -28
+	addiu $sp, $sp, -32
+	sw $s6, 28($sp)
 	sw $s5, 24($sp)
 	sw $s0, 20($sp)
 	sw $s1, 16($sp)
@@ -97,32 +98,34 @@ write_machine_code_next_inst:
 	# 3. Looks like there is another instruction. Call parse_int() with base=16
 	# to convert the instruction into a number, and store it into a register:
 	# YOUR_INSTRUCTIONS_HERE
-	move $a0, $s4
+	move $a0, $v1
 	li $a1, 16
 	jal parse_int
-	move $s4, $v0
+	move $s6, $v0			# $s6: instruction in Hex
 
 	
 	# 4. Check if the instruction needs relocation. If it does not, branch to
 	# the label write_machine_code_to_file:
 	# YOUR_INSTRUCTIONS_HERE
-	move $a0, $s4
+	move $a0, $s6
 	jal inst_needs_relocation
 	beqz $v0, write_machine_code_to_file
 	
 	# 5. Here we handle relocation. Call relocate_inst() with the appropriate
 	# arguments, and store the relocated instruction in the appropriate register:
 	# YOUR_INSTRUCTIONS_HERE
-	move $a0, $s4
+	move $a0, $s6
 	move $a1, $s5
 	move $a2, $s2
 	move $a3, $s3
+	jal relocate_inst
+	move $s6, $v0
 
 write_machine_code_to_file:
 	# 6. Write the instruction into a string buffer via hex_to_str():
 	# YOUR_INSTRUCTIONS_HERE
-	move $a0, $s4
-	move $a1, $s0
+	move $a0, $s6
+	la $a1, hex_buffer
 	jal hex_to_str
 	
 	# 7. Increment the byte offset by the appropriate amount:
@@ -146,6 +149,7 @@ write_machine_code_error:
 	li $v0, -1
 write_machine_code_end:
 	# Don't forget to change this part if you saved more items onto the stack!
+	lw $s6, 28($sp)
 	lw $s5, 24($sp)
 	lw $s0, 20($sp)
 	lw $s1, 16($sp)
@@ -153,7 +157,7 @@ write_machine_code_end:
 	lw $s3, 8($sp)
 	lw $s4, 4($sp)
 	lw $ra, 0($sp)
-	addiu $sp, $sp, 24
+	addiu $sp, $sp, 32
 	jr $ra
 
 ###############################################################################
